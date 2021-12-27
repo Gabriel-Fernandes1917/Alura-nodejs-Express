ddcf8e4da0fc6b3ee1257cd0d1b761bcf5f98484
Gabriel Fernandes <gabgui2001@gmail.com>
2021-12-27 01:50:23 -0300

    .

--- infraestrutura/tables.js
+++ infraestrutura/tables.js
@@ -12,7 +12,8 @@ class tables {
     }
 
     CreateAtendimentos() {
-        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
+        
+        const sql = 'CREATE TABLE IF NOT EXISTS Atendimentos (id int NOT NULL AUTO_INCREMENT, cliente varchar(50) NOT NULL, pet varchar(20), servico varchar(20) NOT NULL, datatime NOT NULL, dataCriacao datetime NOT NULL, status varchar(20) NOT NULL, observacoes text, PRIMARY KEY(id))'
 
         this.conection.query(sql, erro => {
             if(erro){
--- models/atendimentos.js
+++ models/atendimentos.js
@@ -1,11 +1,17 @@
 // sent things to sql
+const moment = require('moment')// momente work with date
+const atendimentos = require('../controllers/atendimentos')
 const conexao = require('../infraestrutura/conection')
 
 class Atendimentos {
     add(atendimento){
+        const dataCriacao = moment().format('YYYY-MM-DD HH:MM:SS')
+        const data = moment(atendimento.data, 'DD/MM/YYYY').format('YYYY-MM-DD HH:MM:SS')//year-moth-day hours:minutes:seconds
+        const atendimentoDatado = {...atendimento, dataCriacao, data} 
+        
         const sql = 'INSERT INTO Atendimentos SET ?' // ? is what add in here go to table
 
-        conexao.query(sql, atendimento, (erro, resultado) => {
+        conexao.query(sql, atendimentoDatado, (erro, resultado) => {
             if(erro){
 
                 console.log(erro)
