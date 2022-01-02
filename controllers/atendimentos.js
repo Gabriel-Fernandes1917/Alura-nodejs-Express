f74c43ddf1c48ae6cbab6f2946cb7af237d17830
Gabriel Fernandes <gabgui2001@gmail.com>
2022-01-01 23:14:17 -0300

    .

--- controllers/atendimentos.js
+++ controllers/atendimentos.js
@@ -1,9 +1,21 @@
+const atendimentos = require('../models/atendimentos')
 const Atendimento = require('../models/atendimentos')
 
 
 module.exports = app =>{
     
-    app.get('/atendimentos',(req, res) => res.send('you stay in atendimentos and doing GET'))
+    app.get('/atendimentos',(req, res) => {
+        
+       Atendimento.lista(res)
+
+    })
+
+    app.get('/atendimentos/:id', (req, res)=>{
+        const id = parseInt(req.params.id)
+
+        Atendimento.buscarPorId(id, res)
+        
+    })
 
     app.post('/atendimentos', (req, res) => {
         //console.log(req.body)
